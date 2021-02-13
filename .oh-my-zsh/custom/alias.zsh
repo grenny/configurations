29b73805e144283254ce054bda1ae52f05f7c151
Grenville Sequeira <gsequeira@athenahealth.com>
2021-02-12 23:48:13 -0500

    Added custom alias file for oh-my-zsh

---
+++ .oh-my-zsh/custom/alias.zsh
@@ -0,0 +1,3 @@
+alias p4streams="_p4streams"
+alias dev="p4streams dev"
+alias review="p4streams review"
--- bin/zsh/.p4streams.sh
+++ bin/zsh/.p4streams.sh
@@ -1,10 +1,10 @@
 # Set ANet P4 Streams Path
 _p4streams() {
-		local streamdir=${1:='dev'};
-		echo "Switching $streamdir stream.";
+	local streamdir=${1:='dev'};
+	echo "Switching $streamdir stream.";
 
-		# Set the Perforce client
-		export REMOTE_P4_CLIENT=${USER}_streams_${streamdir}
-		export REMOTE_P4_HOME=p4/streams/${streamdir}
-		export LOCAL_P4_HOME=p4
+	# Set the Perforce client
+	export REMOTE_P4_CLIENT=${USER}_streams_${streamdir}
+	export REMOTE_P4_HOME=p4/streams/${streamdir}
+	export LOCAL_P4_HOME=p4
 }
